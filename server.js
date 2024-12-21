20498e470a7a2666c5abf46a0d3785310c5fd38c
Djadda kamel <abou_tal_ha@hotmail.com>
2024-12-21 21:07:44 +0100

    Fix session store et MongoStore

--- server.js
+++ server.js
@@ -13,6 +13,8 @@ import MongoStore from 'connect-mongo';
 
 
 
+
+
 dotenv.config();
 
 const app = express()
@@ -38,8 +40,6 @@ app.set('view engine', '.hbs')
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-import MongoStore from 'connect-mongo';
-
 app.use(session({
     secret: process.env.SESSION_SECRET,
     resave: false,
