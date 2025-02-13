990a189fbf1854ea674e4c1554fd8e2eb85bc3c1
Djadda kamel <abou_tal_ha@hotmail.com>
2025-02-13 17:51:32 +0100

    🧹 Nettoyage du modèle utilisateur : suppression des logs et amélioration du hashage

--- model/userModel.js
+++ model/userModel.js
@@ -31,19 +31,12 @@ const userSchema = new mongoose.Schema(
  {timestamps: true},
 );
 
-/*userSchema.pre('save', async function (next) {
-    if (!this.isModified('password')) return next();
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-    next();
-  });*/
+
 
   userSchema.pre('save', async function (next) {
     if (!this.isModified('password')) {
-        console.log("⏩ Le mot de passe n'a pas été modifié, on ne re-hashe pas.");
         return next();
     }
-    console.log("⚡ Hashage du mot de passe en cours...");
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
     next();
